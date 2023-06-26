1b584d53d9252029b919cdd8e4032b63f6169aa1
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2023-06-26 14:40:50 +0300

    Add files via upload

---
+++ demoTest/Test.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+#from sklearn.preprocessing import OneHotEncoder
+
+
+
+
+cam = cv2.VideoCapture(0)
+
+while True:
+	ret, image = cam.read()
+	cv2.imshow('Imagetest',image)
+	k = cv2.waitKey(1)
+	if k != -1:
+		break
+cv2.imwrite('/home/pi/testimage.jpg', image)
+cam.release()
+cv2.destroyAllWindows()
---
+++ demoTest/src/raspicam.h
@@ -0,0 +1,107 @@
+#ifndef _RaspiCam_H_
+#define _RaspiCam_H_
+
+#include <cstdio>
+#include <string>
+#include "rpicamtypes.h"
+
+using namespace std;
+
+
+namespace raspicam{
+
+    namespace _private{
+        class Private_Imp1;
+    }
+
+    class Raspicam
+    {
+    public:
+        Raspicam(/* args */);
+        ~Raspicam();
+    };
+
+    bool open (bool StartCapture=true);
+    bool startCapture;
+
+
+    void setUserCallback(void (*userCallback) (void*), void* data=0);
+
+    bool isOpened(); const;
+    bool grab();
+
+    void retrieve(unsigned char *data, RASPICAM_FORMAT type=RASPICAM_FORMAT_IGNORE);
+
+    unsigned char *getImageBufferData() const;
+
+    size_t getImageufferSize() const;
+
+
+
+    void release();
+    void setFormat(RASPICAM_FORMAT fmt);
+    void setWidth(unsigned int width);
+    void setHeight(unsigned int height);
+    void setCaptureSize(unsigned int width, unsigned int height);
+    void setBrightness(unsigned int brightness);
+    void setSahrpness(int sharpness);
+    void setContrast(int contrast);
+    void setISO(int iso);
+    void setSaturation(int saturation);
+    void setVideoStabilization(bool v);
+    void setExposureCompensasation(int val);
+    void setRotation(int rotation);
+    void setExposure(RASPICAM_EXPOSURE exposure);
+    void setSShutterSpeed(unsigned int ss);
+
+    void setAWB ( RASPICAM_AWB awb );
+    // et specific values for whitebalance. Requires to set seAWB in OFF mode first
+    void setAWB_RB ( float r,float b );//range is 0-1.
+    void setImageEffect ( RASPICAM_IMAGE_EFFECT imageEffect );
+    void setMetering ( RASPICAM_METERING metering );
+    void setHorizontalFlip ( bool hFlip );
+    void setVerticalFlip ( bool vFlip );
+    void setFrameRate ( int frames_per_second );
+    void setSensorMode ( int mode );
+
+    //Accessors
+    RASPICAM_FORMAT getFormat(); const;
+    unsigned int getWidth(); const;
+    unsigned int getHeight(); const;
+    unsigned int getBrightness(); const;
+    unsigned int getRotation(); const;
+    int getISO(); const;
+    int getSharpness(); const;
+    int getContrast(); const;
+    int getSaturation(); const;
+    unsigned int getShutterSpeed(); const;
+    RASPICAM_EXPOSURE getExposure(); const ;
+    RASPICAM_AWB getAWB(); const;
+    float getAWBG_red(); const;
+    float getAWBG_blue(); const;
+    RASPICAM_IMAGE_EFFECT getImageEffect(); const ;
+    RASPICAM_METERING getMetering(); const;
+    int getFrameRate(); const;
+    bool isHorizontallyFlipped(); const ;
+    bool isVerticallyFlipped(); const ;
+
+
+    /** Returns an id of the camera. We assume the camera id is the one of the raspberry
+    *the id is obtained using raspberry serial number obtained in /proc/cpuinfo
+    */
+    string getId(); const;
+
+
+
+    /**Returns the size of the required buffer for the different image types in retrieve
+     */
+    size_t getImageTypeSize ( RASPICAM_FORMAT type ) const;
+    private:
+    _private::Private_Impl *_impl;
+};
+}
+
+}
+
+
+#endif
